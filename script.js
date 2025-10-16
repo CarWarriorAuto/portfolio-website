2a9db7b9a6c9046d96c4287b11b058130e8634cd
CarWarriorAuto <ericmuthuri39@gmail.com>
2025-10-16 10:19:25 +0300

    Added Twitter icon and corresponding color styling

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Portfolio loaded successfully!");
-});
-// Welcome message
-window.onload = function() {
-  alert("Welcome to Erico's Portfolio!");
-};
-
+});s
 // Simple button click effect
 document.querySelector("form button").onclick = function() {
   alert("Thank you for reaching out, Erico will reply soon!");
